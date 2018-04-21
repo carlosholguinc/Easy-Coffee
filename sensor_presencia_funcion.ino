411dcfd23832ead580688a73a233e6879e457521
alanpinon <38591171+alanpinon@users.noreply.github.com>
2018-04-21 13:10:06 -0600

    funcion_sensor_presencia
    devuelve un 0 cuando no hay taza y un 1 cuando si hay

---
+++ sensor_presencia_funcion.ino
@@ -0,0 +1,17 @@
+
+// set pin numbers:
+const int sPin = 2;     // pin digital de entrada
+
+void setup() {
+  // initialize the pushbutton pin as an input:
+  pinMode(sPin, INPUT);
+}
+
+void loop() {
+}
+
+bool presencia()
+{
+  return digitalRead(sPin);
+}
+
