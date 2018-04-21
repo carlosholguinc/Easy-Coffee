484347b83c3b75435db740c862edfb76931a9581
alanpinon <38591171+alanpinon@users.noreply.github.com>
2018-04-21 12:39:32 -0600

    se agrego test de resistencias
    funcion para controlar el manejo de las resistencias para calentar el agua

---
+++ resistencias_calentadoras.ino
@@ -0,0 +1,23 @@
+
+bool band=0;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+// Funcion para activar y desactivar las resistencias calentadoras
+bool encender_res(float valDeseado, float valEntrada)
+{
+  if(valEntrada == valDeseado) band = 1;
+  if(valEntrada == valDeseado-5.0) band = 0;
+  
+  if(valEntrada < valDeseado and band = 0) return HIGH;
+  else return LOW;
+}
+
