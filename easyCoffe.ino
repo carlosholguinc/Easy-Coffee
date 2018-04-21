508e3803d0aabecfa44081564448b9143512c81e
AndresChaparro <38117015+andreschaparroe@users.noreply.github.com>
2018-04-21 13:33:12 -0600

    easyCoffe_v2
    añadiendo funciones

--- easyCoffe.ino
+++ easyCoffe.ino
@@ -1,8 +1,13 @@
 #include <Servo.h>
-char blueToothVal;
-float duration, distance;
+String mensaje;
+float duration, nivel;
+int pinServo= 2;
+String estado = "idle";
+bool vaso = false;
+bool valvula = false;
+bool bandTemp=0;
+int pinCalentador;
 
-int pinServo= 13;
 
 Servo servo;
 void setup()
@@ -15,11 +20,34 @@ void setup()
   delay(3000);
 }
  
- 
 void loop()
 {
+  readWaterLevel();
 
-  // Output pulse with 1ms width on trigPin
+  if(Serial.available())
+  {
+      mensaje=Serial.readString();
+  }
+
+
+  
+  switch(estado){
+    
+   case idle: 
+          turnOffAll();
+          break; 
+   case start: 
+          startAll();
+          break;
+
+    
+   }
+  
+}
+
+void readWaterLevel(void){
+
+   // Output pulse with 1ms width on trigPin
   digitalWrite(A5, HIGH);
   delay(1);
   digitalWrite(A5, LOW);
@@ -27,15 +55,27 @@ void loop()
   // Measure the pulse input in echo pin
   duration = pulseIn(A4, HIGH);
   // Distance is half the duration devided by 29.1 (from datasheet)
-  distance = (duration/2) / 29.1;
+  nivel = (duration/2) / 29.1;
   // if distance less than 0.5 meter and more than 0 (0 or less means over range)
-  Serial.println("2");
   
-  if(Serial.available())
-  {
-    blueToothVal=Serial.read();
-    Serial.println(blueToothVal);
+}
+
+void turnOffAll(void){
+    servo.write(0);
+    digitalWrite(calentar, LOW);
+  }
+
+void startAll(void){
+  servo.write(45);
+    digitalWrite(pinCalentador, HIGH);
   }
 
   
+bool encender_res(float valDeseado, float valEntrada)
+{
+  if(valEntrada == valDeseado) bandTemp = 1;
+  if(valEntrada == valDeseado-5.0) bandTemp = 0;
+  
+  if(valEntrada < valDeseado and bandTemp = 0) return HIGH;
+  else return LOW;
 }
