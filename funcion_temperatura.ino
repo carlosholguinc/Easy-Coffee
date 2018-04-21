3624718a101c1f07046ac69cd6dd1603ef5faa09
alanpinon <38591171+alanpinon@users.noreply.github.com>
2018-04-21 13:14:39 -0600

    funcion temperatura
    devuelve el valor de temperatura en Celcius

---
+++ funcion_temperatura.ino
@@ -0,0 +1,28 @@
+const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
+
+float tempc = 0;        // value read from the pot
+float sensorValue = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+float temperatura ()
+{
+  // lee valor analogico
+  sensorValue = analogRead(analogInPin);
+  
+  // conversion a temperatura
+  tempc = sensorValue * 5.0 * 100.0 / 1024.0;
+
+  //Tiempo de espera para leer adc
+  delay(2);
+  return tempc;
+}
+
